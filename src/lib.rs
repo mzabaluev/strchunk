9733e71e39ac929d68112c187549612187d2b176
Mikhail Zabaluev <mikhail.zabaluev@gmail.com>
2019-11-30 03:22:00 +0200

    Minor rewording in the crate toplevel doc

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,9 @@
 //! UTF-8 string invariants for byte buffers provided by the `bytes` crate.
 //!
 //! The `strchunk` crate builds on the efficient byte containers provided
-//! by crate `bytes`. Its two container types, `StrChunk` and `StrChunkMut`,
-//! wrap around `Bytes` and `BytesMut`, respectively, adding the guarantee
-//! for the content to be valid UTF-8 and therefore making it usable as
+//! by the `bytes` crate. Its two container types, `StrChunk` and `StrChunkMut`,
+//! wrap around `Bytes` and `BytesMut`, respectively, adding a guarantee
+//! for the content to be valid UTF-8 to make it safely usable as
 //! Rust string slices.
 
 #![cfg_attr(feature = "specialization", feature(specialization))]

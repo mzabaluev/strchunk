03619a48dff2478a358d82e912d020f9397744f5
Mikhail Zabaluev <mikhail.zabaluev@gmail.com>
2020-06-03 06:18:38 +0300

    Convert the example to tokio::main and pin-project

--- examples/async_read.rs
+++ examples/async_read.rs
@@ -1,29 +1,22 @@
 use bytes::BytesMut;
 use futures::future;
+use pin_project::{pin_project, project};
 use strchunk::StrChunk;
 use tokio::prelude::*;
-use tokio::runtime::Runtime;
 
 use std::pin::Pin;
 use std::task::{Context, Poll};
 
 const DEFAULT_BUFFER_CAPACITY: usize = 8 * 1024;
 
+#[pin_project]
 pub struct Utf8Reader<R> {
+    #[pin]
     inner: R,
     buf: BytesMut,
 }
 
-impl<R: Unpin> Unpin for Utf8Reader<R> {}
-
 impl<R> Utf8Reader<R> {
-    fn split_borrows(self: Pin<&mut Self>) -> (Pin<&mut R>, &mut BytesMut) {
-        unsafe {
-            let this = self.get_unchecked_mut();
-            (Pin::new_unchecked(&mut this.inner), &mut this.buf)
-        }
-    }
-
     pub fn new(inner: R) -> Self {
         Utf8Reader {
             inner,
@@ -48,11 +41,13 @@ fn extract_utf8_if_read_ok<'a>(
 }
 
 impl<R: AsyncRead> Utf8Reader<R> {
+    #[project]
     pub fn poll_read_utf8(
         self: Pin<&mut Self>,
         cx: &mut Context,
     ) -> Poll<io::Result<StrChunk>> {
-        let (inner, buf) = self.split_borrows();
+        #[project]
+        let Utf8Reader { inner, buf } = self.project();
         debug_assert!(buf.capacity() >= 4);
         inner
             .poll_read_buf(cx, buf)
@@ -60,12 +55,11 @@ impl<R: AsyncRead> Utf8Reader<R> {
     }
 }
 
-async fn forward_all<R, W>(input: R, mut output: W) -> io::Result<()>
-where
-    R: AsyncRead + Unpin,
-    W: AsyncWrite + Unpin,
-{
-    let mut reader = Utf8Reader::new(input);
+#[tokio::main]
+async fn main() -> io::Result<()> {
+    let s: &[_] = b"Hello, world!\n";
+    let mut out = io::stdout();
+    let mut reader = Utf8Reader::new(s);
     loop {
         let chunk =
             future::poll_fn(|cx| Pin::new(&mut reader).poll_read_utf8(cx))
@@ -73,15 +67,9 @@ where
         if chunk.is_empty() {
             break;
         } else {
-            output.write_all(chunk.as_str().as_bytes()).await?;
+            out.write_all(chunk.as_str().as_bytes()).await?;
         }
     }
-    output.flush().await
-}
-
-fn main() {
-    let s: &[_] = b"Hello, world!\n";
-    let out = io::stdout();
-    let mut runtime = Runtime::new().unwrap();
-    runtime.block_on(forward_all(s, out)).unwrap();
+    out.flush().await?;
+    Ok(())
 }
